5ba9ab2c6c7e6f5ea4d9029e19bb3ae75671ef66
Gian Crescini Santillan <giansantillan18@gmail.com>
2025-02-20 09:48:21 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -94,7 +94,7 @@ export default defineConfig((config) => {
       host: true,
       allowedHosts: [
         'localhost',
-        'boltdiy.mydomain.no'
+        'bolt.fusiontech.asia'
       ],
     },
     build: {
